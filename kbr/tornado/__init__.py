ab96bb597a9eb4a1b790cace3f35942c82bc1880
brugger <kim.brugger@uib.no>
2022-06-10 23:05:04 +0200

    url-encode function

--- kbr/tornado/__init__.py
+++ kbr/tornado/__init__.py
@@ -307,6 +307,14 @@ def url_unescape(uri:str) -> str:
     
     return tornado.escape.url_unescape( uri )
 
+
+def url_escape(uri:str, plus=True) -> str:
+    if uri is None:
+        return uri
+    
+    return tornado.escape.url_escape( uri, plus=plus )
+
+
 def set_token(new_token:str):
     global token
     token = new_token
